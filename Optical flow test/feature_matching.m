c1dfe774b701f3e1e18b65aeb12d0c1ba889e664
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2015-05-05 14:28:58 +0200

    changes to feature detection

--- Optical flow test/feature_matching.m
+++ Optical flow test/feature_matching.m
@@ -5,8 +5,20 @@ clc, clear
 I1 = imread('0000000010.png');
 I2 = imread('0000000011.png');
 
-points1 = detectHarrisFeatures(I1);
-points2 = detectHarrisFeatures(I2);
+minQ = 0.0001;                        %   MinQuality, default 0.01
+filtS = 3;                          %   FilterSize, default 5
+roi = [1 1 size(I1,2) size(I1,1)];  %   ROI, default [1 1 size(I1,2) size(I1,1)]
+
+
+points1 = detectHarrisFeatures(I1, 'MinQuality', minQ, ...
+                                    'FilterSize', filtS, ...
+                                    'ROI', roi);
+                                
+points2 = detectHarrisFeatures(I2, 'MinQuality', minQ, ...
+                                    'FilterSize', filtS, ...
+                                    'ROI', roi);
+
+
 
 [features1, valid_points1] = extractFeatures(I1, points1);
 [features2, valid_points2] = extractFeatures(I2, points2);

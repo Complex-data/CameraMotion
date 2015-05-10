8982c4d6594b22df4d32d1b2ee574e418e073a14
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2015-05-10 12:46:27 +0200

    fmaeorf

---
+++ FeatureDetection/main.m
@@ -0,0 +1,14 @@
+clc, clear
+
+I1 = imread('0000000000.png');
+I2 = imread('0000000001.png');
+
+mQ = 0.0001;
+filt = 3;
+ROI = [1 1 size(I1,2) size(I1,1)];
+
+[x, y, u, v, mp1, mp2] = returnHarrisVectors(I1, I2, mQ, filt, ROI);
+
+quiver(x,y, -u, -v);
+
+% figure; showMatchedFeatures(I1, I2, mp1, mp2);

1032f9d30cbe625d52c6eb26766cec98a7c830c4
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2015-05-14 00:37:14 +0200

    moamer

--- DisparityAndFeatureDetection/main.m
+++ DisparityAndFeatureDetection/main.m
@@ -53,3 +53,22 @@ closedF = conv2(closed, h, 'same');
 imshow(closedF, [0, 64]);
 colormap jet
 
+%% Feature detection
+
+
+I1 = imread(['../ImageData/LeftData/', indexToImageName(index)]);
+I2 = imread(['../ImageData/LeftData/', indexToImageName(index+1)]);
+
+
+mQ = 0.0001;
+filt = 3;
+ROI = [1 200 750 176];
+% ROI = [1 1 size(I1,2) size(I1,1)];
+% ROI = [1 round(size(I1,2)/2) size(I1,2) size(I1,1)];
+tic
+[x, y, u, v, mp1, mp2] = returnHarrisVectors(I1, I2, mQ, filt, ROI);
+toc
+% quiver(x,y, -u, -v);
+
+figure(1);
+showMatchedFeatures(I1, I2, mp1, mp2);

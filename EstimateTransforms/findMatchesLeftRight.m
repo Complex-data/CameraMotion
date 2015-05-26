05c04a77914ac5bf943242225856bb4e1458c75d
Cyianor <crazyvision@gmail.com>
2015-05-26 12:07:02 +0200

    Extract circle features with our own features

--- EstimateTransforms/findMatchesLeftRight.m
+++ EstimateTransforms/findMatchesLeftRight.m
@@ -1,19 +1,19 @@
-function [ind, met] = matchLeftRight(f1, f2, vp1, vp2, M, thr, uniq)
+function [ind, met] = findMatchesLeftRight(fl, fr, vpl, vpr, M, thr, uniq)
     % Assume that there are fewer features in f1. If this is not the case
     % than swap their roles
-    s1 = length(f1);
-    s2 = length(f2);
+    s1 = length(fl);
+    s2 = length(fr);
     
     swap = false;    
     if s2 == min(s1, s2)
         % Swap features
-        temp = f2;
-        f2 = f1;
-        f1 = temp;
+        temp = fr;
+        fr = fl;
+        fl = temp;
         % Swap valid points
-        temp = vp2;
-        vp2 = vp1;
-        vp1 = temp;
+        temp = vpr;
+        vpr = vpl;
+        vpl = temp;
         % Swap sizes
         temp = s2;
         s2 = s1;
@@ -23,12 +23,12 @@ function [ind, met] = matchLeftRight(f1, f2, vp1, vp2, M, thr, uniq)
     end
     
     % Sort the valid points and features by the row coordinates
-    [~, i1_rows] = sort(vp1(:, 2));
-    vp1 = vp1(i1_rows, :);
-    f1 = f1(i1_rows, :);
-    [~, i2_rows] = sort(vp2(:, 2));
-    vp2 = vp2(i2_rows, :);
-    f2 = f2(i2_rows, :);
+    [~, i1_rows] = sort(vpl(:, 2));
+    vpl = vpl(i1_rows, :);
+    fl = fl(i1_rows, :);
+    [~, i2_rows] = sort(vpr(:, 2));
+    vpr = vpr(i2_rows, :);
+    fr = fr(i2_rows, :);
     
     ind = zeros(s1, 2, 'uint16');
     met = zeros(s1, 1);
@@ -38,11 +38,11 @@ function [ind, met] = matchLeftRight(f1, f2, vp1, vp2, M, thr, uniq)
     for k = 1:s1
         lowest_sad = thr;
         increase = true;
-        r1 = vp1(k, 2);
+        r1 = vpl(k, 2);
         % As long as we are too low it is not interesting. We are matching
         % left right, so the features should be epipolar. Give it a
         % possible error of plus/minus one pixel
-        while l <= s2 && vp2(l, 2) < r1 - 1
+        while l <= s2 && vpr(l, 2) < r1 - 1
             l = l + 1;
         end
         % Save this for later
@@ -50,11 +50,11 @@ function [ind, met] = matchLeftRight(f1, f2, vp1, vp2, M, thr, uniq)
         
         % We found a feature marker at a row position higher than r1 - 1.
         % Let's see if it also not too high already
-        while l <= s2 && vp2(l, 2) <= r1 + 1
+        while l <= s2 && vpr(l, 2) <= r1 + 1
             % Here comes the check if we didn't maybe wander off too far
-            if abs(vp1(k, 1) - vp2(l, 1)) <= M
+            if abs(vpl(k, 1) - vpr(l, 1)) <= M
                 % Let's do the feature matching
-                temp_sad = sum(abs(f1(k, :) - f2(l, :)));
+                temp_sad = sum(abs(fl(k, :) - fr(l, :)));
                 if temp_sad < lowest_sad 
                     % We found a match
                     lowest_sad = temp_sad;
@@ -94,6 +94,31 @@ function [ind, met] = matchLeftRight(f1, f2, vp1, vp2, M, thr, uniq)
         ind = ind(1:num, :);
         met = met(1:num);
     end
+    
+%     % Do a sanity check and remove the matches with negative disparity
+%     % (left u position lower than on the right)
+%     ind2 = zeros(size(ind), 'uint16');
+%     met2 = zeros(size(met));
+%     num = 0;
+%     if swap
+%         for k = 1:length(ind)
+%             if vpr(ind(k, 1), 1) >= vpl(ind(k, 2), 1)
+%                 num = num + 1;
+%                 ind2(num, :) = ind(k, :);
+%                 met2(num) = met(k);
+%             end
+%         end
+%     else
+%         for k = 1:length(ind)
+%             if vpl(ind(k, 1), 1) >= vpr(ind(k, 2), 1)
+%                 num = num + 1;
+%                 ind2(num, :) = ind(k, :);
+%                 met2(num) = met(k);
+%             end
+%         end
+%     end
+%     ind = ind2(1:num, :);
+%     met = met2(1:num);
        
     % If it is wished for ensure that only unique matches show up
     if uniq
